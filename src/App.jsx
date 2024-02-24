b21b7b939c4a7a0ceb49fc6a8bd3bcf2538f30a9
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-24 16:16:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file for styling
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here, such as sending a request to authenticate the user
+    console.log('Logging in with:', username, password);
+  };
+
+  return (
+    <div className='container'>
+      <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+
+export default App;
